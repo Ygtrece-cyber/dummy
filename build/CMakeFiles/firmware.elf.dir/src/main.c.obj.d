be9696254f183a7f263a90b56568295a174cbe03
Ygtrece-cyber <2119969546@qq.com>
2026-07-22 18:59:31 +0800

    Add files via upload

---
+++ build/CMakeFiles/firmware.elf.dir/src/main.c.obj.d
@@ -0,0 +1,23 @@
+CMakeFiles/firmware.elf.dir/src/main.c.obj: \
+ C:/Users/ThinkBOOK/robot_firmware/src/main.c \
+ C:/Program\ Files\ (x86)/Arm/GNU\ Toolchain\ mingw-w64-i686-arm-none-eabi/lib/gcc/arm-none-eabi/15.2.1/include/stdint.h \
+ C:/Program\ Files\ (x86)/Arm/GNU\ Toolchain\ mingw-w64-i686-arm-none-eabi/arm-none-eabi/include/stdint.h \
+ C:/Program\ Files\ (x86)/Arm/GNU\ Toolchain\ mingw-w64-i686-arm-none-eabi/arm-none-eabi/include/machine/_default_types.h \
+ C:/Program\ Files\ (x86)/Arm/GNU\ Toolchain\ mingw-w64-i686-arm-none-eabi/arm-none-eabi/include/sys/features.h \
+ C:/Program\ Files\ (x86)/Arm/GNU\ Toolchain\ mingw-w64-i686-arm-none-eabi/arm-none-eabi/include/_newlib_version.h \
+ C:/Program\ Files\ (x86)/Arm/GNU\ Toolchain\ mingw-w64-i686-arm-none-eabi/arm-none-eabi/include/sys/_intsup.h \
+ C:/Program\ Files\ (x86)/Arm/GNU\ Toolchain\ mingw-w64-i686-arm-none-eabi/arm-none-eabi/include/sys/_stdint.h \
+ C:/Program\ Files\ (x86)/Arm/GNU\ Toolchain\ mingw-w64-i686-arm-none-eabi/arm-none-eabi/include/string.h \
+ C:/Program\ Files\ (x86)/Arm/GNU\ Toolchain\ mingw-w64-i686-arm-none-eabi/arm-none-eabi/include/_ansi.h \
+ C:/Program\ Files\ (x86)/Arm/GNU\ Toolchain\ mingw-w64-i686-arm-none-eabi/arm-none-eabi/include/newlib.h \
+ C:/Program\ Files\ (x86)/Arm/GNU\ Toolchain\ mingw-w64-i686-arm-none-eabi/arm-none-eabi/include/sys/config.h \
+ C:/Program\ Files\ (x86)/Arm/GNU\ Toolchain\ mingw-w64-i686-arm-none-eabi/arm-none-eabi/include/machine/ieeefp.h \
+ C:/Program\ Files\ (x86)/Arm/GNU\ Toolchain\ mingw-w64-i686-arm-none-eabi/arm-none-eabi/include/sys/reent.h \
+ C:/Program\ Files\ (x86)/Arm/GNU\ Toolchain\ mingw-w64-i686-arm-none-eabi/arm-none-eabi/include/_ansi.h \
+ C:/Program\ Files\ (x86)/Arm/GNU\ Toolchain\ mingw-w64-i686-arm-none-eabi/lib/gcc/arm-none-eabi/15.2.1/include/stddef.h \
+ C:/Program\ Files\ (x86)/Arm/GNU\ Toolchain\ mingw-w64-i686-arm-none-eabi/arm-none-eabi/include/sys/cdefs.h \
+ C:/Program\ Files\ (x86)/Arm/GNU\ Toolchain\ mingw-w64-i686-arm-none-eabi/arm-none-eabi/include/sys/_types.h \
+ C:/Program\ Files\ (x86)/Arm/GNU\ Toolchain\ mingw-w64-i686-arm-none-eabi/arm-none-eabi/include/machine/_types.h \
+ C:/Program\ Files\ (x86)/Arm/GNU\ Toolchain\ mingw-w64-i686-arm-none-eabi/arm-none-eabi/include/sys/lock.h \
+ C:/Program\ Files\ (x86)/Arm/GNU\ Toolchain\ mingw-w64-i686-arm-none-eabi/arm-none-eabi/include/sys/string.h \
+ C:/Users/ThinkBOOK/robot_firmware/inc/uart_driver.h
